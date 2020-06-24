bfbf51179202c905a751210d967bd87f0b03d75c
Deepak <deepakrout@hotmail.com>
2020-06-24 12:24:21 -0400

    Initial Commit

---
+++ Dicee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee
+//
+//  Created by Deepak Rout on 6/24/20.
+//  Copyright © 2020 Deepak Rout. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
